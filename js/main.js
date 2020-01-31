624cc1c64eb5a436b810aa4401d2d3ea0fb4078c
rick wilcoxen <60274472+rickwilcoxen@users.noreply.github.com>
2020-01-31 09:11:02 -0500

    created js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped" + " " + cardOne);
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+
+cardsInPlay.push('cardOne');
+
+console.log("user flipped queen");
+
+const cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+
+ console.log("user flipped king")
+
+ if (cardsInPlay.length === 2) {
+result = (cardsInPlay[0] === cardsInPlay[2])
+alert('You found a match!')
+ }
+ else
+ 	alert('Sorry, try again.')
+
